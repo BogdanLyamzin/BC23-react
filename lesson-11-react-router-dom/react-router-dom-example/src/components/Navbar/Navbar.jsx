726f8e57028be33990e31eb7e68190098edd91c1
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-09-01 12:20:35 +0300

    finish lesson 11

---
+++ lesson-11-react-router-dom/react-router-dom-example/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+import NavbarMenu from "./NavbarMenu/NavbarMenu";
+
+import styles from "./navbar.module.scss";
+
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className="container">
+                <div className={styles.row}>
+                    <Link to="/">Logo</Link>
+                    <NavbarMenu />
+                    <span>Cart:</span>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ lesson-11-react-router-dom/react-router-dom-example/src/pages/PostsPage/PostsPage.jsx
@@ -0,0 +1,9 @@
+const PostsPage = () => {
+    return (
+        <div className="container">
+            <h1 className="page-title">Posts page</h1>
+        </div>
+    )
+}
+
+export default PostsPage;
