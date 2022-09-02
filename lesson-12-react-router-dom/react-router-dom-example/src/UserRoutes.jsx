1d1ddd6475136d829417d8e6354e976f3d06a1ef
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-09-02 12:04:25 +0300

    finish lesson 12

---
+++ lesson-12-react-router-dom/react-router-dom-example/src/UserRoutes.jsx
@@ -0,0 +1,28 @@
+import { lazy, Suspense } from "react";
+import {Routes, Route} from "react-router-dom";
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const PostsPage = lazy(() => import('./pages/PostsPage/PostsPage'));
+const MyBooksPage = lazy(() => import('./pages/MyBooksPage/MyBooksPage'));
+const SinglePostPage = lazy(() => import("./pages/SinglePostPage/SinglePostPage"));
+const CommentsPage = lazy(() => import("./pages/CommentsPage/CommentsPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+
+const UserRoutes = () => {
+    return (
+        <Suspense fallback={<p>...Load page</p>}>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/posts" element={<PostsPage />} />
+                <Route path="/my-books" element={<MyBooksPage />} />
+                <Route path="/posts/:id" element={<SinglePostPage />}>
+                    <Route path="comments" element={<CommentsPage />} />
+                </Route>
+                
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </Suspense>
+    )
+}
+
+export default UserRoutes;
---
+++ lesson-12-react-router-dom/react-router-dom-example/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import Posts from "../../components/Posts/Posts";
+
+const HomePage = () => {
+    return (
+        <div className="container">
+            <h1 className="page-title">Home page</h1>
+            <Posts />
+        </div>
+    )
+}
+
+export default HomePage;
